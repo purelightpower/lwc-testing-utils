49075a4a569b11215a89a4d4dbf468ac795dc6d5
Morgan Billingsley <mbillingsley@purelightpower.com>
2023-01-31 14:37:56 -0700

    Added first shadow dom parser test

---
+++ tests/shadowDomParset.test.js
@@ -0,0 +1,27 @@
+import { createElement } from "lwc";
+import HelloWorld from "c/helloWorld";
+
+describe("ShadowDomParser tests", () => {
+    let parser;
+
+    beforeEach(() => {
+        let element = createElement("c-hello-world", {
+            is: HelloWorld
+        });
+        document.body.appendChild(element);
+        parser = new ShadowDomParser(element);
+    });
+
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        parser = null;
+    });
+
+    it("Find an immediate child by query selector", () => {
+        let child = parser.findOne(".title");
+
+        expect(child.textContent).toBe("Hello World!");
+    });
+});
