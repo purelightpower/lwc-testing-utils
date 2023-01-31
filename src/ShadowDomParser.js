56573fe42c3cdebb2658426f5d2f0a0030a23adb
Morgan Billingsley <mbillingsley@purelightpower.com>
2023-01-31 14:57:24 -0700

    Added find all method to dom parser

--- src/ShadowDomParser.js
+++ src/ShadowDomParser.js
@@ -7,12 +7,21 @@ class ShadowDomParser {
             return this.findOneInImmediateChildren(selector);
     }
 
+    findAll(selector) {
+        let fromParent = this.findAllInParent(selector);
+        return fromParent;
+    }
+
     findOneInImmediateChildren(selector) {
         let element = this.parent.shadowRoot.querySelector(selector);
         if (element) {
             return element;
         }
-        throw new Error(`No element with a the ${selector} selector could be found in any of the component's immediate children.`);
+    }
+
+    findAllInParent(selector) {
+        let nodes = this.parent.shadowRoot.querySelectorAll(selector);
+        return Array.from(nodes);
     }
 
 }
