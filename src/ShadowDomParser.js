770de621a87c69aae474723856e3090e1bc4ea9a
Morgan Billingsley <mbillingsley@purelightpower.com>
2023-01-31 15:37:29 -0700

    Added ability to find nested component

--- src/ShadowDomParser.js
+++ src/ShadowDomParser.js
@@ -1,10 +1,16 @@
+const ALL_CHILDREN = "*";
 class ShadowDomParser {
     constructor(parent) {
         this.parent = parent;
+        this.nestedComponents = [];
     }
 
     findOne(selector) {
+        try {
             return this.findOneInImmediateChildren(selector);
+        } catch (_) {
+            return this.findOneInNestedComponents(selector);
+        }
     }
 
     findAll(selector) {
@@ -22,9 +28,53 @@ class ShadowDomParser {
         );
     }
 
+    findOneInNestedComponents(selector) {
+        this.conditionallySetNestedComponents();
+        for (let component of this.nestedComponents) {
+            try {
+                return component.findOne(selector);
+            } catch (_) {
+                /** */
+            }
+        }
+        throw new Error(
+            `No element with a the ${selector} selector could be found.`
+        );
+    }
+
     findAllInParent(selector) {
         let nodes = this.parent.shadowRoot.querySelectorAll(selector);
         return Array.from(nodes);
+    }
+
+    conditionallySetNestedComponents() {
+        if (this.nestedComponents.length < 1) {
+            this.setNestedComponents();
+        }
+    }
+
+    setNestedComponents() {
+        this.nestedComponents = [];
+        let children = this.getChildren();
+        this.setNestedComponentsFromChildren(children);
+    }
+
+    getChildren() {
+        return this.parent.shadowRoot.querySelectorAll(ALL_CHILDREN);
+    }
+
+    setNestedComponentsFromChildren(children) {
+        for (let child of children) {
+            if (ShadowDomParser.isCustomComponent(child)) {
+                let parser = new ShadowDomParser(child);
+                this.nestedComponents.push(parser);
+            }
+        }
+    }
+
+    static isCustomComponent(element) {
+        let lowerCaseTagName = element.tagName.toLowerCase();
+        return lowerCaseTagName.startsWith("c-");
     }
 
 }
