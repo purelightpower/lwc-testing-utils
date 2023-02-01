f2bd5ccae26c77677876d0cda2ac3948c0035699
Morgan Billingsley <mbillingsley@purelightpower.com>
2023-02-01 14:09:39 -0700

    Updated signup form for address input test

--- tests/lwc/signupForm/signupForm.js
+++ tests/lwc/signupForm/signupForm.js
@@ -1,9 +1,28 @@
 import { LightningElement } from "lwc";
 
 export default class SignupForm extends LightningElement {
+    home = {
+        street: "",
+        city: "",
+        province: "",
+        postalCode: "",
+        country: "",
+    };
     sendMail = false;
     plans = [{ label: "Free", value: "free" }, { label: "Premium", value: "premium" }];
     plan = "";
+    billingAddressIsSame = true;
+    billing = {
+        street: "",
+        city: "",
+        province: "",
+        postalCode: "",
+        country: "",
+    };
+
+    handleHomeChange(event) {
+        this.home = event.detail;
+    }
 
     handleMailChange(event) {
         this.sendMail = event.detail.checked;
@@ -11,6 +30,14 @@ export default class SignupForm extends LightningElement {
 
     handlePlanChange(event) {
         this.plan = event.detail.value;
+    }
+
+    handleBillingAddressIsSameChange(event) {
+        this.billingAddressIsSame = event.detail.checked;
+    }
+
+    handleBillingChange(event) {
+        this.billing = event.detail;
     }
 
     get isPaid() {
