ea8e81a805bdc0fbfed4d33819626397eeed208f
Morgan Billingsley <mbillingsley@purelightpower.com>
2023-02-02 14:29:20 -0700

    Created the waiter class

---
+++ src/Waiter.js
@@ -0,0 +1,37 @@
+import EventEmitter from "events";
+
+class Waiter {
+    constructor(element) {
+        this.emitter = new EventEmitter();
+        this.element = element;
+        this.#resolve = Promise.resolve;
+        this.#listenerCallback = this.#handleChange.bind(this);
+    }
+
+    listenOnce(resolve) {
+        this.emitter.once("change", this.stop.bind(this));
+        this.start(resolve);
+    }
+
+    start(resolve) {
+        this.#resolve = resolve;
+        this.element.addEventListener(CHANGE_EVENT, this.#listenerCallback);
+    }
+
+    stop() {
+        this.element.removeEventListener(CHANGE_EVENT, this.#listenerCallback);
+    }
+    
+    #handleChange(event) {
+        if (this.isDoneWaiting(event)) {
+            this.emitter.emit("change", event);
+            this.#resolve();
+        }
+    }
+
+    isDoneWaiting() {
+        return true;
+    }
+}
+
+export default Waiter;
