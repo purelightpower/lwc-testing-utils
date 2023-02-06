1e4e229ae8be0322bf7847d136b865e41e186d39
Morgan Billingsley <mbillingsley@purelightpower.com>
2023-02-06 12:39:16 -0700

    Export lightning address input waiter

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import ShadowDomParser from "./src/ShadowDomParser";
 import FormChanger from "./src/FormChanger";
 import LightningInputWaiter from "./src/waiters/LightningInputWaiter";
+import LightningAddressInputWaiter from "./src/waiters/LightningAddressInputWaiter";
 
-export { ShadowDomParser, FormChanger, LightningInputWaiter };
+export {
+    ShadowDomParser,
+    FormChanger,
+    LightningInputWaiter,
+    LightningAddressInputWaiter,
+};
